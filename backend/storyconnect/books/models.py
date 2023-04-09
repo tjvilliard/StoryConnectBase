a3e49e4c0c86b8195916ed704a0a4a43b14fc61b
novellaalvina <112059794+novellaalvina@users.noreply.github.com>
2023-04-09 17:19:32 -0600

    fixed relationship between book and character and more modification fixings. Finalised models.py

--- backend/storyconnect/books/models.py
+++ backend/storyconnect/books/models.py
@@ -6,36 +6,43 @@ class Account(models.Model):
     username = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
     DOB = models.DateField()
-
-# # class Library(models.Model):
-# #     reading = 
+    
+    def __str__(self):
+        return self.username
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     cover = models.ImageField(upload_to='covers/')
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
     synopsis = models.TextField(max_length=1000)
     tagging = models.CharField(max_length=30)
-    chsheet = models.ForeignKey(Character, on_delete=models.CASCADE, null=True, blank=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
     
+class Library(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1)
+
 class Chapter(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    chapter_title = models.CharField(max_length=100)
     # title = models.CharField(max_length=100)
     content = models.TextField()
 
     def __str__(self):
-        return self.title
+        return self.chapter_title
 
 class Manuscript_Title(models.Model):
     titlepage = models.TextField()
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     bookcover = models.ImageField(upload_to='covers/') #?
+    
+    def __str__(self):
+        return self.book.title
 
 class Outline(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
@@ -43,7 +50,9 @@ class Outline(models.Model):
     scene = models.CharField(max_length=50)
     scenecontent = models.CharField(max_length=50)
 
-# needs fixing
+    def __str__(self):
+        return self.chapter.chapter_title
+
 class Character(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
@@ -67,3 +76,8 @@ class Location(models.Model):
 
     def __str__(self):
         return self.name
+
+class General(models.Model):
+    accID = models.ForeignKey(Account, on_delete=models.CASCADE)
+    bookID = models.ForeignKey(Book, on_delete=models.CASCADE)
+    libID = models.ForeignKey(Library, on_delete=models.CASCADE)
