72330ffa547fd5f48f671e2be910fdaa176bcc26
tjvilliard <tjvilliard@gmail.com>
2023-04-10 09:20:31 -0600

    Updated CICD file. Updated models. Updated views.

--- backend/storyconnect/books/views.py
+++ backend/storyconnect/books/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
+
+def create_book(request):
+    return render(request, 'books/create_book.html')
+
+def create_chapter(request):
+    return render(request, 'books/create_chapter.html')
+
+def create_character(request):
+    return render(request, 'books/create_character.html')
+
+def create_location(request):
+    return render(request, 'books/create_location.html')
+
