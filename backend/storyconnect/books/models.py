83fee15d4babde42170bcd3c7c07c29706eb8b7b
novellaalvina <112059794+novellaalvina@users.noreply.github.com>
2023-04-09 19:01:38 -0600

    make language, target audience, copyrights and book status into choices

--- backend/storyconnect/books/models.py
+++ backend/storyconnect/books/models.py
@@ -11,21 +11,42 @@ class Account(models.Model):
         return self.username
 
 class Book(models.Model):
+    LANGUAGES = [
+        (1, "English"),
+        (2, "Indonesian")
+    ]
+    TARGET_AUDIENCES = [
+        (1, "Young Adult (13-18 years old)"), 
+        (2, "New Adult (18-25 years old)"),
+        (3, "Adult (25+ years old)")
+    ]
+    # taken from chapterly.com and wattpad.com
+    COPYRIGHTS = [
+        (1, "All Rights Reserved: No part of this publication may be reproduced, stored or transmitted in any form or by any means, electronic, mechanical, photocopying, recording, scanning, or otherwise without written permission from the publisher. It is illegal to copy this book, post it to a website, or distribute it by any other means without permission."), 
+        (2, "Public Domain: This story is open source for the public to use for any purposes."), 
+        (3, "Creative Commons (CC) Attribution: Author of the story has some rights to some extent and allow the public to use this story for purposes like translations or adaptations credited back to the author.")
+    ]
+    
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    language = models.TextChoices("English", "Indonesian")
+    target_audience = models.CharField("Young Adult (13-18 years old)", "New Adult (18-25 years old)", "Adult (25+ years old)")
     cover = models.ImageField(upload_to='covers/')
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
     synopsis = models.TextField(max_length=1000)
     tagging = models.CharField(max_length=30)
+    copyright = models.CharField("All Rights Reserved: No part of this publication may be reproduced, stored or transmitted in any form or by any means, electronic, mechanical, photocopying, recording, scanning, or otherwise without written permission from the publisher. It is illegal to copy this book, post it to a website, or distribute it by any other means without permission.", 
+        "Public Domain: This story is open source for the public to use for any purposes.", 
+        "Creative Commons (CC) Attribution: Author of the story has some rights to some extent and allow the public to use this story for purposes like translations or adaptations credited back to the author.")
 
     def __str__(self):
         return self.title
     
 class Library(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    status = models.CharField(max_length=1)
+    status = models.IntegerChoices("Reading", "In the Works", "Archived")
 
 class Chapter(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
@@ -39,7 +60,7 @@ class Chapter(models.Model):
 class Manuscript_Title(models.Model):
     titlepage = models.TextField()
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    bookcover = models.ImageField(upload_to='covers/') #?
+    bookcover = models.ImageField(upload_to='covers/') 
     
     def __str__(self):
         return self.book.title
@@ -79,5 +100,5 @@ class Location(models.Model):
 
 class General(models.Model):
     accID = models.ForeignKey(Account, on_delete=models.CASCADE)
-    bookID = models.ForeignKey(Book, on_delete=models.CASCADE)
+    bookID = models.ForeignKey(Book, on_delete=models.CASCADE) # user's work(s)
     libID = models.ForeignKey(Library, on_delete=models.CASCADE)
