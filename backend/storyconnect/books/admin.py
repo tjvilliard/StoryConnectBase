803b3c02e8c7d8f464bc786d8d9fbc9c69ea6056
tjvilliard <tjvilliard@gmail.com>
2023-04-13 09:36:20 -0600

    Registered models books/admin.py

--- backend/storyconnect/books/admin.py
+++ backend/storyconnect/books/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'cover', 'synopsis', 'date_created', 'date_modified', 'owner')
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):   
+    list_display = ('book', 'title', 'content')
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ('book', 'name', 'description', 'image')
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('book', 'name', 'description')  
