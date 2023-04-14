176caa61869ad3a158f717c4976dc348c2072713
tjvilliard <tjvilliard@gmail.com>
2023-04-14 09:49:15 -0600

    Merge branch 'develop' into backend-merge-develop
    # Conflicts:
    #	backend/storyconnect/storyconnect/settings.py

--- backend/storyconnect/books/tests.py
+++ backend/storyconnect/books/tests.py
@@ -58,7 +58,7 @@ class BookViewSetTestCase(APITestCase):
 
     def setUp(self):
         # Create a user
-        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.user = User.objects.create_user(username='testuser', password='testpassword', email="test@mail.com")
 
         # Authenticate the user
         self.client.force_authenticate(user=self.user)
@@ -76,7 +76,7 @@ class BookViewSetTestCase(APITestCase):
         )
 
         # URL for the books API
-        self.books_url = reverse('book-list')
+        self.books_url = '/api/books/'
     
     def test_create_book(self):
         data = {
@@ -114,3 +114,113 @@ class BookViewSetTestCase(APITestCase):
         response = self.client.delete(book_detail_url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Book.objects.count(), 0)
+
+
+
+
+
+class UserBookCreationTestCase(APITestCase):
+    """
+    Test suite for User and Book Properties
+    """
+    def setUp(self):
+
+        # Book.objects.create(title= "Demo item 2",description= "This is a description for demo 2",price= 700,stock= 15)
+
+        # self.items = Book.objects.all()
+
+        # create user
+        self.user = User.objects.create_user(
+            username='testuser1', 
+            password='this_is_a_test',
+            email='testuser1@test.com'
+        )
+        # create a book for the user
+        self.userbook = Book.objects.create(
+            title="Red Queen", 
+            author = "Victoria Aveyard", 
+            owner = self.user,
+            language=1,target_audience = 1,
+            synopsis = "This is the book synopsis.",
+            copyright = 1, 
+            titlepage = "This is the book title page.")
+        
+
+        #The app uses token authentication
+        # self.token = Token.objects.get(user = self.user)
+        # self.client = APIClient()
+        
+        # #We pass the token in all calls to the API
+        # self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+    # testing how many books are there essentially testing perform_create
+    def test_book_count(self):
+        '''
+        test BookViewSet list method
+        '''
+        self.assertEqual(Book.objects.count(), 2)
+        response = self.client.get('/books/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    # testing perform query to only show the author's books
+    def test_author_all_books(self):
+        authorbooks = Book.objects.filter(owner = self.user)
+        for b in authorbooks:
+            response = self.client.get(f'/books/{b.owner}/') # not sure
+            self.assertEqual(response.status_code, status.HTTP_200_OK)                           
+    
+    def test_getting_book_properties(self):
+        attr_dict = {
+            'title'               : "Red Queen",
+            'author'              : "Victoria Aveyard",
+            'language'            : 1,
+            'target_audience'     : 1,
+            'synopsis'            : "This is the book synopsis.",
+            'copyright'           : 1,
+            'titlepage'           : "This is the book title page."}
+        # self.assertEqual(self.userbook.title, title)
+        # self.assertEqual(self.userbook.author, author)
+        # self.assertEqual(self.userbook.language, language)
+        # self.assertEqual(self.userbook.target_audience, target_audience)
+        # self.assertEqual(self.userbook.synopsis, synopsis)
+        # self.assertEqual(self.userbook.copyright, copyright_chosen)
+        # self.assertEqual(self.userbook.titlepage, titlepage)
+        self.assertEqual(self.userbook.__getattribute__.__dict__(), attr_dict)
+        # self.userbook.queryset
+
+    def test_retrieve_all_book(self):
+        bookview = BookViewSet.retrieve()
+        attr_dict = {
+            'title'               : "Red Queen",
+            'author'              : "Victoria Aveyard",
+            'language'            : 1,
+            'target_audience'     : 1,
+            'synopsis'            : "This is the book synopsis.",
+            'copyright'           : 1,
+            'titlepage'           : "This is the book title page."}
+        self.assertEqual(bookview.data, attr_dict)
+    
+    # testing to miss the non-null attribute when creating or something
+    def test_filling_in_with_invalid_input_for_attributes(self):
+
+        return 0
+
+    def test_updating_book_properties(self):
+        self.userbook.title = "Dune"
+        bookview = BookViewSet.update()
+        attr_dict = {
+            'title'               : "Dune",
+            'author'              : "Victoria Aveyard",
+            'language'            : 1,
+            'target_audience'     : 1,
+            'synopsis'            : "This is the book synopsis.",
+            'copyright'           : 1,
+            'titlepage'           : "This is the book title page."}        
+        self.assertEqual(bookview.data, attr_dict)   
+
+    def test_getting_a_specific_book(self):
+        dune = Book.objects.filter(title="Dune")
+        response = self.client.get(f'/books/{dune.id}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+content="A hobbit is a small human-like creature that lives in a hole in the ground. They are very peaceful and like to eat and drink."
