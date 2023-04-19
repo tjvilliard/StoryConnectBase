28eac3716dde3e19e26d6bbf5cefc908a582502d
novellaalvina <112059794+novellaalvina@users.noreply.github.com>
2023-04-19 04:45:42 -0600

    run makemigrations and add rest framework json api to settings.py

--- backend/storyconnect/storyconnect/settings.py
+++ backend/storyconnect/storyconnect/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django_filters',
     'django_extensions',
     'rest_framework',
+    'rest_framework_json_api',
     'rest_framework.authtoken',
     'books',
     'core',
