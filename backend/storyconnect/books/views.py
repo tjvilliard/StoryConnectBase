6c03134c5a1a25c45fea5117468c60c795327313
tjvilliard <tjvilliard@gmail.com>
2023-04-20 16:19:11 -0600

    Removed get_queryset from books viewset. Added update function. Changed return to JsonResponse.

--- backend/storyconnect/books/views.py
+++ backend/storyconnect/books/views.py
@@ -22,13 +22,22 @@ class BookViewSet(viewsets.ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return JsonResponse(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-    def get_queryset(self):
-        return Book.objects.filter(owner=self.request.user.id)
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return JsonResponse(serializer.data)
+
+    
+
+
 
 # def create_book(request):
 #     if request.method == 'POST':
