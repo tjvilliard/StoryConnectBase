483afba77145d441f8344e1efa28126655cd3545
Dallin Burton <dallin.burton@me.com>
2023-04-23 15:55:23 -0600

    Create Loading schema for WritingHomePage, with code only needing to call the api to get it working

--- frontend/storyconnect/lib/Pages/writing_home/view.dart
+++ frontend/storyconnect/lib/Pages/writing_home/view.dart
@@ -5,48 +5,124 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:storyconnect/Models/models.dart';
 import 'package:storyconnect/Pages/writing_home/writing_home_bloc.dart';
 
-class WritingHomeView extends StatelessWidget {
+class WritingHomeView extends StatefulWidget {
   const WritingHomeView({Key? key}) : super(key: key);
+
+  @override
+  WritingHomeState createState() => WritingHomeState();
+}
+
+class WritingHomeState extends State<WritingHomeView> {
+  final TextEditingController textController = TextEditingController();
+  bool initialLoad = true;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (initialLoad) {
+        initialLoad = false;
+        final writingHomeBloc = context.read<WritingHomeBloc>();
+        writingHomeBloc.add(GetBooksEvent());
+      }
+    });
+  }
+
+  void create(BuildContext context) {
+    final writingHomeBloc = context.read<WritingHomeBloc>();
+    writingHomeBloc.add(
+      CreateBookEvent(title: textController.text.trim()),
+    );
+    textController.text = "";
+  }
+
+  void openBook(WritingHomeBloc state) {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: BlocBuilder<WritingHomeBloc, WritingHomeStruct>(
-            buildWhen: (previous, current) {
-      return previous.books != current.books;
-    }, builder: (context, state) {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 20),
-            child: FilledButton.icon(
-                onPressed: () {
-                  final writingHomeBloc = context.read<WritingHomeBloc>();
-                  writingHomeBloc.add(CreateBookEvent());
-                },
-                icon: Icon(FontAwesomeIcons.plus),
-                label: Text("CreateBook")),
-          ),
-          Flexible(
-              child: ListView.separated(
-                  itemCount: state.books.length,
+      body: BlocConsumer<WritingHomeBloc, WritingHomeStruct>(
+        listener: (context, state) {
+          if (state.bookToNavigate != null) {
+            final url = "/writer/${state.bookToNavigate!.bookId}";
+            Beamer.of(context).beamToNamed(url);
+          }
+        },
+        buildWhen: (previous, current) {
+          final loadingDiff = previous.loadingStruct != current.loadingStruct;
+          final bookDiff = previous.books != current.books;
+          return bookDiff || loadingDiff;
+        },
+        builder: (context, state) {
+          int addLoading = 0;
+          if (state.loadingStruct.isLoading) {
+            addLoading++;
+          }
+
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: textController,
+                        onSubmitted: (_) => state.loadingStruct.isLoading
+                            ? null
+                            : create(context),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: FilledButton.icon(
+                        onPressed: () => state.loadingStruct.isLoading
+                            ? null
+                            : create(context),
+                        icon: Icon(FontAwesomeIcons.plus),
+                        label: Text("CreateBook"),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                child: ListView.separated(
+                  itemCount: state.books.length + addLoading,
                   separatorBuilder: (context, index) {
                     return SizedBox(
                       height: 10,
                     );
                   },
                   itemBuilder: (context, index) {
+                    if (index == state.books.length) {
+                      return Center(
+                          child: Column(
+                        children: [
+                          if (state.loadingStruct.message != null)
+                            Padding(
+                                padding: EdgeInsets.symmetric(vertical: 15),
+                                child: Text(state.loadingStruct.message!)),
+                          CircularProgressIndicator()
+                        ],
+                      ));
+                    }
+
                     Book book = state.books[index];
                     return FilledButton(
                       onPressed: () {
-                        Beamer.of(context).beamToNamed("writer/$index");
+                        final writingHomeBloc = context.read<WritingHomeBloc>();
+                        writingHomeBloc.add(OpenBookEvent(bookId: book.id));
                       },
                       child: Text(book.title),
                     );
-                  }))
-        ],
-      );
-    }));
+                  },
+                ),
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
--- frontend/storyconnect/lib/Pages/writing_home/writing_home_bloc.dart
+++ frontend/storyconnect/lib/Pages/writing_home/writing_home_bloc.dart
@@ -2,21 +2,64 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:storyconnect/Models/models.dart';
 import 'package:storyconnect/Pages/writing_home/writing_repository.dart';
 
-abstract class WritingHomeEvent {}
+abstract class WritingHomeEvent {
+  bool isLoading;
+  WritingHomeEvent({required this.isLoading});
+}
 
-class RecievedBooksEvent extends WritingHomeEvent {
-  final List<Book> books;
-  RecievedBooksEvent({required this.books});
+class GetBooksEvent extends WritingHomeEvent {
+  GetBooksEvent() : super(isLoading: true);
 }
 
 class CreateBookEvent extends WritingHomeEvent {
-  CreateBookEvent();
+  String title;
+  CreateBookEvent({required this.title}) : super(isLoading: true);
+}
+
+class OpenBookEvent extends WritingHomeEvent {
+  int bookId;
+  OpenBookEvent({required this.bookId}) : super(isLoading: true);
+}
+
+class BookToNavigate {
+  final int bookId;
+  BookToNavigate({required this.bookId});
+}
+
+class LoadingStruct {
+  final bool isLoading;
+  final String? message;
+
+  LoadingStruct({required this.isLoading, this.message});
+
+  factory LoadingStruct.loading(bool isLoading) {
+    return LoadingStruct(isLoading: isLoading);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is LoadingStruct &&
+        other.isLoading == isLoading &&
+        other.message == message;
+  }
+
+  @override
+  int get hashCode => isLoading.hashCode ^ message.hashCode;
+
+  @override
+  String toString() {
+    return 'LoadingStruct(isLoading: $isLoading, message: $message)';
+  }
 }
 
 class WritingHomeStruct {
   final List<Book> books;
-
-  WritingHomeStruct({required this.books});
+  final BookToNavigate? bookToNavigate;
+  final LoadingStruct loadingStruct;
+  WritingHomeStruct(
+      {required this.books, required this.loadingStruct, this.bookToNavigate});
 }
 
 typedef WritingHomeEmitter = Emitter<WritingHomeStruct>;
@@ -24,16 +67,37 @@ typedef WritingHomeEmitter = Emitter<WritingHomeStruct>;
 class WritingHomeBloc extends Bloc<WritingHomeEvent, WritingHomeStruct> {
   late final WritingHomeRepository repository;
   WritingHomeBloc({required this.repository})
-      : super(WritingHomeStruct(books: [])) {
-    on<RecievedBooksEvent>((event, emit) => updateBooks(event, emit));
+      : super(WritingHomeStruct(
+            books: [], loadingStruct: LoadingStruct(isLoading: false))) {
+    on<GetBooksEvent>((event, emit) => updateBooks(event, emit));
     on<CreateBookEvent>((event, emit) => createBook(event, emit));
+    on<OpenBookEvent>((event, emit) => openBook(event, emit));
   }
 
-  void updateBooks(WritingHomeEvent event, WritingHomeEmitter emit) {}
-  void createBook(CreateBookEvent event, WritingHomeEmitter emit) async {
-    Book newBook = await repository.createBook();
+  void updateBooks(WritingHomeEvent event, WritingHomeEmitter emit) async {
+    emit(WritingHomeStruct(
+        books: state.books,
+        loadingStruct: LoadingStruct.loading(event.isLoading)));
+    List<Book> books = await repository.getBooks();
+    emit(WritingHomeStruct(
+        books: books, loadingStruct: LoadingStruct.loading(false)));
+  }
 
-    // Update the state with the new book
-    emit(WritingHomeStruct(books: [...state.books, newBook]));
+  void createBook(CreateBookEvent event, WritingHomeEmitter emit) async {
+    emit(WritingHomeStruct(
+        books: state.books,
+        loadingStruct: LoadingStruct(
+            isLoading: event.isLoading, message: "Creating book")));
+    Book newBook = await repository.createBook(title: event.title);
+    emit(WritingHomeStruct(
+        books: [...state.books, newBook],
+        loadingStruct: LoadingStruct.loading(false)));
+  }
+
+  void openBook(OpenBookEvent event, WritingHomeEmitter emit) async {
+    emit(WritingHomeStruct(
+        books: state.books,
+        loadingStruct: LoadingStruct.loading(false),
+        bookToNavigate: BookToNavigate(bookId: event.bookId)));
   }
 }
