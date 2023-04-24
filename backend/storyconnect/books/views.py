ac4dd76fad87515d8a6443cd77f902ef282a878e
novellaalvina <112059794+novellaalvina@users.noreply.github.com>
2023-04-24 02:17:34 -0600

    rename update to put to get recognised in api testing in postman

--- backend/storyconnect/books/views.py
+++ backend/storyconnect/books/views.py
@@ -28,20 +28,24 @@ class BookViewSet(viewsets.ModelViewSet):
     # def perform_create(self, serializer):
     #     serializer.save(owner=self.request.user)
 
-    def update(self, request, *args, **kwargs):
+    def put(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
         return JsonResponse(serializer.data)
-
+    
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
+    
     @action(detail=True, methods=['get'])
     def get_chapters(self, request, pk=None):
         book = self.get_object()
         chapters = book.get_chapters()
         serializer = ChapterSerializer(chapters, many=True)
-        return Response(serializer.data)
+        return JsonResponse(serializer.data)
     
 class ChapterViewSet(viewsets.ModelViewSet):
     queryset = Chapter.objects.all()
