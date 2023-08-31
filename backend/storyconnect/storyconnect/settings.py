3dd7ac609bd58e5f45af6b756a0fbd46b13e85d9
tjvilliard <tjvilliard@gmail.com>
2023-08-31 12:42:36 -0600

    Added firebase credentials and initialization for authentication and storage in settings.py

--- backend/storyconnect/storyconnect/settings.py
+++ backend/storyconnect/storyconnect/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import firebase_admin
+from firebase_admin import storage
+from firebase_admin import credentials
 # from dotenv import load_dotenv
 
 # load_dotenv()
@@ -172,6 +175,27 @@ FIREBASE_AUTH_URI = os.environ.get('FIREBASE_AUTH_URI')
 FIREBASE_TOKEN_URI = os.environ.get('FIREBASE_TOKEN_URI')
 FIREBASE_AUTH_PROVIDER_X509_CERT_URL = os.environ.get('FIREBASE_AUTH_PROVIDER_X509_CERT_URL')
 FIREBASE_CLIENT_X509_CERT_URL = os.environ.get('FIREBASE_CLIENT_X509_CERT_URL')
+
+cred = credentials.Certificate(
+    {
+        "type": "service_account",
+        "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+        "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+        "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+        "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://accounts.google.com/o/oauth2/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_CERT_URL"),
+    }
+)
+
+
+# cred = firebase_admin.credentials.Certificate('storyconnect/firbase_credentials.json', options = {'storageBucket' : 'gs://storyconnect-9c7dd.appspot.com'})
+default_app = firebase_admin.initialize_app(cred, name='[DEFAULT]', options = {'storageBucket' : 'gs://storyconnect-9c7dd.appspot.com'})
+
+FIREBASE_BUCKET = storage.bucket(app=default_app, name= 'storyconnect-9c7dd.appspot.com')
 
 REST_FRAMEWORK = {'DEFAULT_AUTHENTICATION_CLASSES' : ['rest_framework.authentication.SessionAuthentication',
                                                     'core.authentication.FirebaseAuthentication']}
