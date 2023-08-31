4ae1a685f6c80f0451f5ff0c47885a55f58e2934
David Harrington <david.harrington2095@gmail.com>
2023-08-30 19:41:25 -0600

    Committing Firebase google files

--- frontend/storyconnect/lib/main.dart
+++ frontend/storyconnect/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:storyconnect/Services/Beamer/beamer_locations.dart';
 import 'package:storyconnect/theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 final routerDelegate = BeamerDelegate(
   locationBuilder: BeamerLocationBuilder(
@@ -16,6 +17,8 @@ final routerDelegate = BeamerDelegate(
 );
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
