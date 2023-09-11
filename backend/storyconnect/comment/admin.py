7cbda8df58c1a5b84c4db5c9a4cc6ff029df1d32
tjvilliard <tjvilliard@gmail.com>
2023-09-11 10:35:49 -0600

    Registered models to admin

--- backend/storyconnect/comment/admin.py
+++ backend/storyconnect/comment/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+
+@admin.register(admin.ModelAdmin)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'selection', 'comment', 'parent')
+
+@admin.register(admin.ModelAdmin)
+class AnnotationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'selection', 'annotation')
+
+@admin.register(admin.ModelAdmin)
+class HighlightAdmin(admin.ModelAdmin):
+    list_display = ('user', 'selection', 'color')
+    
