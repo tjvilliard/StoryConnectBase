d67a6ca88a31dd9608a573ba139c679bde5c6982
tjvilliard <tjvilliard@gmail.com>
2023-09-11 10:53:58 -0600

    Updated urls.py with comment urls

--- backend/storyconnect/storyconnect/urls.py
+++ backend/storyconnect/storyconnect/urls.py
@@ -18,11 +18,15 @@ from django.urls import path
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 from books import views as books_views
+from comment import views as comment_views
 
 
 router = routers.DefaultRouter()
 router.register(r'api/books', books_views.BookViewSet)
 router.register(r'api/chapters', books_views.ChapterViewSet)
+router.register(r'api/highlights', comment_views.HighlightViewSet)
+router.register(r'api/annotations', comment_views.AnnotationViewSet)
+router.register(r'api/comments', comment_views.CommentViewSet)
 
 
 urlpatterns = router.urls
