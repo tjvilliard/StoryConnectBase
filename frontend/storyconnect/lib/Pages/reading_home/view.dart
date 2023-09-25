6c99271c1b2d19179bfc7d2638dc7292b3d981a1
David Harrington <david.harrington2095@gmail.com>
2023-09-25 10:24:45 -0600

    Writing Home Stuff

---
+++ frontend/storyconnect/lib/Pages/reading_home/view.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class ReadingHomeView extends StatefulWidget {
+  const ReadingHomeView({Key? key}) : super(key: key);
+}
