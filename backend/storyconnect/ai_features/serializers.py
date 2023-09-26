aea8c2372f39ec93e192b95ecbda8858ed0ae8b1
tjvilliard <tjvilliard@gmail.com>
2023-09-26 10:21:51 -0600

    Preliminary ai serializers done

---
+++ backend/storyconnect/ai_features/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+
+class RoadUnblockerRequestSerializer(serializers.Serializer):
+    chapter = serializers.IntegerField()
+    selection = serializers.CharField()
+    question = serializers.CharField()
+
+class RoadUnblockerSuggestionSerializer(serializers.Serializer):
+    offset_start = serializers.IntegerField()
+    offset_end = serializers.IntegerField()
+
+    original = serializers.CharField(required=False)
+    suggested_change = serializers.CharField()
+
+class RoadUnblockerResponseSerializer(serializers.Serializer):
+    suggestions = RoadUnblockerSuggestionSerializer(many=True)
+    message = serializers.CharField()
+
+
+    
