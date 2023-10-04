f0295b936ec8c468351689c74a604be3bea1920b
tjvilliard <tjvilliard@gmail.com>
2023-10-04 14:54:07 -0600

    moved some stuff in cont. checker

--- backend/storyconnect/ai_features/continuity_checker.py
+++ backend/storyconnect/ai_features/continuity_checker.py
@@ -15,10 +15,10 @@ class ContinuityChecker:
     MAX_TOKENS = 512
     TEMPERATURE = 0.2
 
-    # continuity statement parameters
-    stmt_task = "Generate a number of simple statements about each named entity in the text that are directly supported by the text. The statements only describe the named entities and do not describe actions within the text."
-    stmt_instructions = "Format the response in XML, the first chunk will be for characters and the second will be for places. The header for each subsection is the entity's name, and what follows are the statements about the entities. The statements must start with the entity's name and not a pronoun. If the entity is more than one word, replace the space with a hyphen in the element tag."
-    stmt_ex = "<Characters>\n<John-Doe>\nJohn Doe has blue eyes.\nJohn Doe is tall.\n</John-Doe>\n<Jane-Doe>\nJane Doe has blonde hair.\nJane Doe is 23 years old.\n</Jane-Doe>\n</Characters>\n<Locations>\n<New-York>\nNew York is a city.\nNew York is in the United States.\n</New-York>\n\n</Locations>"
+    # # continuity statement parameters
+    # stmt_task = "Generate a number of simple statements about each named entity in the text that are directly supported by the text. The statements only describe the named entities and do not describe actions within the text."
+    # stmt_instructions = "Format the response in XML, the first chunk will be for characters and the second will be for places. The header for each subsection is the entity's name, and what follows are the statements about the entities. The statements must start with the entity's name and not a pronoun. If the entity is more than one word, replace the space with a hyphen in the element tag."
+    # stmt_ex = "<Characters>\n<John-Doe>\nJohn Doe has blue eyes.\nJohn Doe is tall.\n</John-Doe>\n<Jane-Doe>\nJane Doe has blonde hair.\nJane Doe is 23 years old.\n</Jane-Doe>\n</Characters>\n<Locations>\n<New-York>\nNew York is a city.\nNew York is in the United States.\n</New-York>\n\n</Locations>"
 
     # continuity comparison parameters
     
@@ -26,8 +26,25 @@ class ContinuityChecker:
 
 
     @staticmethod
+    def _strip_xml(text):
+        tree = etree.fromstring(text)
+        
+        statements = ""
+        for child in tree:
+            for subchild in child:
+                statements += subchild.text.strip()
+
+        return statements
+
+
+    @staticmethod
     def _statement_prompt(text):
-        return f"Text: {text}\n\nTask: {ContinuityChecker.stmt_task}\n\nInstructions: {ContinuityChecker.stmt_instructions}\n\n Example: \n{ContinuityChecker.stmt_ex}"
+        # continuity statement parameters
+        stmt_task = "Generate a number of simple statements about each named entity in the text that are directly supported by the text. The statements only describe the named entities and do not describe actions within the text."
+        stmt_instructions = "Format the response in XML, the first chunk will be for characters and the second will be for places. The header for each subsection is the entity's name, and what follows are the statements about the entities. The statements must start with the entity's name and not a pronoun. If the entity is more than one word, replace the space with a hyphen in the element tag."
+        stmt_ex = "<Characters>\n<John-Doe>\nJohn Doe has blue eyes.\nJohn Doe is tall.\n</John-Doe>\n<Jane-Doe>\nJane Doe has blonde hair.\nJane Doe is 23 years old.\n</Jane-Doe>\n</Characters>\n<Locations>\n<New-York>\nNew York is a city.\nNew York is in the United States.\n</New-York>\n\n</Locations>"
+
+        return f"Text: {text}\n\nTask: {stmt_task}\n\nInstructions: {stmt_instructions}\n\n Example: \n{stmt_ex}"
     
     
     def create_statementsheet(self, text):
@@ -36,7 +53,7 @@ class ContinuityChecker:
         
         
         prompt = ContinuityChecker._statement_prompt(text)
-        self.last_response = openai.Completion.create(model = self.MODEL,
+        self.last_response = openai.Completion.create(model = self.BASE_MODEL,
                                                 prompt = prompt,
                                                 max_tokens = self.MAX_TOKENS,
                                                 temperature = self.TEMPERATURE,
@@ -47,32 +64,36 @@ class ContinuityChecker:
         formatted_text = "<Statements>\n" + body.strip() + "\n</Statements>"
         return formatted_text
     
+    def filter_statementsheet(self, statementsheet):
+        filter_prompt = """Given the following set of statements, remove 
+        any statements that do not define the entities they are describing. Remove any statemnents that 
+        describe what is currently happening. 
+        Example: Given "John Doe is tall." and "John Doe is running." remove "John Doe is running."\n"""
+        filter_prompt += statementsheet
+
+        self.last_response = openai.Completion.create(model = self.BASE_MODEL,
+                                                      prompt = filter_prompt,
+                                                      max_tokens = self.MAX_TOKENS,
+                                                      temperature = self.TEMPERATURE,
+                                                      )
+        return self.last_response['choices'][0]['text'].strip()
+
     def compare_statementsheets(self, s_old, s_new):
-        s_old = ContinuityChecker.strip_xml(s_old)
-        s_new = ContinuityChecker.strip_xml(s_new)
+        s_old = ContinuityChecker._strip_xml(s_old)
+        s_new = ContinuityChecker._strip_xml(s_new)
 
         # print(s_old)
         # print('\n')
         # print(s_new)
-        comp_input = f"The following statements are about previously written text: \n {s_old} \n The following statements are about new text: \n {s_new}\n"
-        comp_instructions = "Identify any contradictions between the old text and the new text. Do not list any contradictions that are not supported by the statements. Stop when appropriate."
+        comp_input = f"The following statements are about previously written text: \n {s_old} \n The next statements are about new text: \n {s_new}\n"
+        comp_instructions = "Identify any contradictions between the old text and the new text. Briefly summarize the contradicitons. Do not say anything about changes that dont contain contradictions. Do not use complicated formatting. Be brief. Stop when appropriate."
         prompt = comp_input + comp_instructions
         
         self.last_response = openai.Completion.create(model = self.BASE_MODEL,
                                                       prompt = prompt,
                                                         max_tokens = self.MAX_TOKENS ,
-                                                        temperature = self.TEMPERATURE * 5,
+                                                        temperature = self.TEMPERATURE * 2,
         )
         
         return self.last_response['choices'][0]['text'].strip()
 
-    @staticmethod
-    def strip_xml(text):
-        tree = etree.fromstring(text)
-        
-        statements = ""
-        for child in tree:
-            for subchild in child:
-                statements += subchild.text.strip()
-
-        return statements
