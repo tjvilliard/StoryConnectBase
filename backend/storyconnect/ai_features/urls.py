194423e29f42d93f3839770a43668296cc4017a4
Dallin Burton <dallin.burton@me.com>
2023-10-18 10:34:50 -0600

    implement placeholder apis so that we're at least 'calling' apis

--- backend/storyconnect/ai_features/urls.py
+++ backend/storyconnect/ai_features/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
-from .views import RoadUnblockerRequestView, RoadUnblockerSuggestionView
+from .views import RoadUnblockerRequestView, RoadUnblockerSuggestionView, NarrativeElementView, ContinuityView
 
 
 app_name = "ai_features"
 
 urlpatterns = [
-    path('request/', RoadUnblockerRequestView.as_view(), name="roadunblocker_request"),
-    path('suggestion/', RoadUnblockerSuggestionView.as_view(), name="roadunblocker_suggestion"),
+    path('api/request/', RoadUnblockerRequestView.as_view(), name="roadunblocker_request"),
+    path('api/suggestion/', RoadUnblockerSuggestionView.as_view(), name="roadunblocker_suggestion"),
+    path('api/narrative_elements/<int:book_id>/', NarrativeElementView.as_view(), name="narrative_element"),
+    path('api/continuities/<int:chapter_id>/', ContinuityView.as_view(), name="continuity")
+
 ]
