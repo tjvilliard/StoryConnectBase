b25c07c541e2002fc1d2b5068d7f4a7a413114f2
David Harrington <david.harrington2095@gmail.com>
2023-10-25 02:24:16 -0600

    Added Dependency for Date-Time Conversions and formatting, Created basic Tag UI, tags are semi-static.

--- frontend/storyconnect/lib/Pages/reading_hub/components/detailed_book_item.dart
+++ frontend/storyconnect/lib/Pages/reading_hub/components/detailed_book_item.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 import 'package:storyconnect/Models/models.dart';
+import 'package:storyconnect/theme.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 class newDescriptBookItem extends StatelessWidget {
   final Book book;
 
   /// Creates a clickable book item filled in with details based on the provided book.
   newDescriptBookItem({required this.book});
+
+  String yyMMddDateTime(DateTime dateTime) {
+    final DateFormat formatter = DateFormat.yMd();
+    String formatted = formatter.format(dateTime);
+    return formatted;
+  }
 
   Widget _topRightHandDetail({required Widget textItem}) {
     return Align(
@@ -83,8 +92,91 @@ class newDescriptBookItem extends StatelessWidget {
             )
           ],
         ),
-        Container(), // Bottom Row Items: Tag Items
+        Padding(
+            padding: EdgeInsets.only(left: 31.0, right: 31.0),
+            child: Container(
+                child: Wrap(
+              runSpacing: 4.0,
+              spacing: 4.0,
+              children: [
+                TagWidget(
+                  tagCategory: TagCategories.genre,
+                  tagText: "Tragic Romance",
+                ),
+                TagWidget(
+                  tagCategory: TagCategories.genre,
+                  tagText: "Dystopia ",
+                ),
+                TagWidget(
+                  tagCategory: TagCategories.genre,
+                  tagText: "Science Fiction ",
+                ),
+                TagWidget(
+                  tagCategory: TagCategories.genre,
+                  tagText: "Science Fantasy ",
+                ),
+                TagWidget(
+                  tagCategory: TagCategories.genre,
+                  tagText: "Drama",
+                ),
+                book.language == null
+                    ? SizedBox.shrink()
+                    : TagWidget(
+                        tagCategory: TagCategories.filter,
+                        tagText: book.language!,
+                      ),
+                TagWidget(
+                  tagCategory: TagCategories.filter,
+                  tagText: "Modified: ${this.yyMMddDateTime(book.modified)}",
+                ),
+                TagWidget(
+                  tagCategory: TagCategories.filter,
+                  tagText: "Created: ${this.yyMMddDateTime(book.created)}",
+                ),
+                TagWidget(
+                  tagCategory: TagCategories.manual,
+                  tagText: "Humor",
+                ),
+              ],
+            ))), // Bottom Row Items: Tag Items
       ],
     );
+  }
+}
+
+const Color charcoalBlue = Color(0xFF28536B);
+const Color rosyRed = Color.fromARGB(255, 196, 64, 84);
+const Color yellow = Color.fromARGB(255, 171, 169, 59);
+
+enum TagCategories {
+  genre(charcoalBlue),
+  filter(rosyRed),
+  manual(yellow);
+
+  const TagCategories(this.color);
+  final Color color;
+}
+
+///
+class TagWidget extends StatelessWidget {
+  final TagCategories tagCategory;
+  final String tagText;
+
+  const TagWidget({required this.tagCategory, required this.tagText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        constraints: BoxConstraints(minHeight: 20),
+        decoration: BoxDecoration(
+          border: Border.all(width: .75, color: this.tagCategory.color),
+          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 3.0),
+          child: Text(this.tagText,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodySmall),
+        ));
   }
 }
