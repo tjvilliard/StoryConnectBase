29e203ac39f41414b3e7538436d5812c62ed7cee
Dallin Burton <dallin.burton@me.com>
2023-11-08 11:26:41 -0700

    add todo

--- backend/storyconnect/ai_features/continuity_checker.py
+++ backend/storyconnect/ai_features/continuity_checker.py
@@ -86,7 +86,8 @@ class ContinuityChecker:
         # print('\n')
         # print(s_new)
         comp_input = f"The following statements are about previously written text: \n {s_old} \n The next statements are about new text: \n {s_new}\n"
-        comp_instructions = "Identify any contradictions between the old text and the new text. Briefly summarize the contradicitons. Do not say anything about changes that dont contain contradictions. Do not use complicated formatting. Be brief. Stop when appropriate."
+        # TODO: unused variable
+        # comp_instructions = "Identify any contradictions between the old text and the new text. Briefly summarize the contradicitons. Do not say anything about changes that dont contain contradictions. Do not use complicated formatting. Be brief. Stop when appropriate."
         comp_instructions_list = "List any contradictions between the descriptions in the old text versus the new. If an entity is not mentioned in the new text, ignore it. If no contradicions exist, say 'NONE'."
         prompt = comp_input + comp_instructions_list
         
