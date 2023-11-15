01ed31ce035c52d39d8ae9f9630abb33f56f4672
tjvilliard <tjvilliard@gmail.com>
2023-11-15 10:32:30 -0700

    Generated plugins

--- frontend/storyconnect/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/storyconnect/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,20 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import device_info_plus
-import file_selector_macos
 import firebase_auth
 import firebase_core
 import path_provider_foundation
-import url_launcher_macos
-import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
