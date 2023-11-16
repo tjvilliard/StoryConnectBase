02281db403676d6de90f45e8c7c99605b3349528
tjvilliard <tjvilliard@gmail.com>
2023-11-16 11:23:48 -0700

    Updated ai_features serializers for uid and field matching

--- backend/storyconnect/ai_features/serializers.py
+++ backend/storyconnect/ai_features/serializers.py
@@ -3,25 +3,27 @@ from rest_framework import serializers
 
 class RoadUnblockerRequestSerializer(serializers.Serializer):
     chapter = serializers.IntegerField()
-    selection = serializers.CharField()
+    selection = serializers.CharField(allow_blank=True, allow_null=True)
     question = serializers.CharField()
 
 class RoadUnblockerSuggestionSerializer(serializers.Serializer):
+    uid = serializers.CharField()
     offset_start = serializers.IntegerField()
     offset_end = serializers.IntegerField()
-
     suggestion = serializers.CharField()
     original = serializers.CharField(required=False)
     suggested_change = serializers.CharField()
 
 class RoadUnblockerResponseSerializer(serializers.Serializer):
+    uid = serializers.CharField()
     suggestions = RoadUnblockerSuggestionSerializer(many=True)
     message = serializers.CharField()
 
 class ContinuityItemSerializer(serializers.Serializer):
+    uid = serializers.CharField()
     content = serializers.CharField()
     chapter_id = serializers.IntegerField()
-    uuid = serializers.CharField()
+    
 
 
 class ContinuityCheckerResponseSerializer(serializers.Serializer):
