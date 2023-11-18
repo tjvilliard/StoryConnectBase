fea1fd90d3e3a5f9db37c10b6eeecef5fedb4cd4
Dallin Burton <dallin.burton@me.com>
2023-11-17 19:00:42 -0700

    implement backend of profile_image_upload

--- backend/storyconnect/books/serializers.py
+++ backend/storyconnect/books/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
-from .models import Book, Chapter, Library, NarrativeElement, NarrativeElementType, NarrativeElementAttribute, NarrativeElementAttributeType
+from .models import (
+    Book,
+    Chapter,
+    Library,
+    NarrativeElement,
+    NarrativeElementType,
+    NarrativeElementAttribute,
+    NarrativeElementAttributeType,
+)
+
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = "__all__"
+
 
 class ChapterSerializer(serializers.ModelSerializer):
     # book = serializers.PrimaryKeyRelatedField(queryset=Book.objects.all(), many=False)
@@ -12,12 +22,14 @@ class ChapterSerializer(serializers.ModelSerializer):
         model = Chapter
         fields = "__all__"
 
+
 class LibrarySerializer(serializers.ModelSerializer):
     # book = serializers.PrimaryKeyRelatedField(queryset=Book.objects.all(), many=False)
     # reader = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=False)
     class Meta:
         model = Library
-        exclude = ['reader']
+        exclude = ["reader"]
+
 
 class LibraryBookSerializer(serializers.ModelSerializer):
     class Meta:
@@ -32,20 +44,25 @@ class NarrativeElementSerializer(serializers.ModelSerializer):
         model = NarrativeElement
         fields = "__all__"
 
+
 class NarrativeElementTypeSerializer(serializers.ModelSerializer):
     # book = serializers.PrimaryKeyRelatedField(queryset=Book.objects.all(), many=False)
     class Meta:
         model = NarrativeElementType
         fields = "__all__"
-    
+
+
 class NarrativeElementAttributeSerializer(serializers.ModelSerializer):
     # book = serializers.PrimaryKeyRelatedField(queryset=Book.objects.all(), many=False)
     class Meta:
         model = NarrativeElementAttribute
         fields = "__all__"
 
+
 class NarrativeElementAttributeTypeSerializer(serializers.ModelSerializer):
     # book = serializers.PrimaryKeyRelatedField(queryset=Book.objects.all(), many=False)
+    serializers.ImageField(use_url=True)
+
     class Meta:
         model = NarrativeElementAttributeType
         fields = "__all__"
