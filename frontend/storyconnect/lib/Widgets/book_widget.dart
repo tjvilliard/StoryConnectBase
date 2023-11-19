c358c26790c8175fdcef144986b51b00e24eb25c
Dallin Burton <dallin.burton@me.com>
2023-11-19 14:33:45 -0700

    Update the book widget image

--- frontend/storyconnect/lib/Widgets/book_widget.dart
+++ frontend/storyconnect/lib/Widgets/book_widget.dart
@@ -25,11 +25,14 @@ class BookWidgetState extends State<BookWidget> {
 
   // build a rectangular placeholder for the book cover
   Widget _imagePlaceHolder() {
-    return SizedBox(
-      height: 150,
-      width: 100,
-      child: Icon(Icons.book, size: 100),
-    );
+    return Column(children: [
+      SizedBox(
+        height: 150,
+        width: 100,
+        child: Icon(Icons.book, size: 100),
+      ),
+      bookTitle()
+    ]);
   }
 
   Future<void> get_image(String? relativePath) async {
@@ -51,42 +54,70 @@ class BookWidgetState extends State<BookWidget> {
     }
   }
 
+  Widget bookTitle() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          widget.book.title,
+          overflow: TextOverflow.ellipsis,
+          style: Theme.of(context).textTheme.labelMedium!.copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
+        ),
+        if (widget.book.authorName != null)
+          Text(
+            widget.book.authorName ?? "No name",
+            style: Theme.of(context).textTheme.labelSmall!.copyWith(color: Colors.white),
+          ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.all(2),
-        child: Card(
-          margin: EdgeInsets.all(0),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              if (url != null && url!.isNotEmpty)
-                ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: ImageLoader(
-                      url: url!,
-                      fit: BoxFit.cover,
-                      constraints: BoxConstraints(maxWidth: 100, maxHeight: 160),
-                    )),
-              if (url == null || url!.isEmpty) _imagePlaceHolder(),
-              Flexible(
-                  child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8),
-                      child: Text(
-                        widget.book.title,
-                        overflow: TextOverflow.ellipsis,
-                        style: Theme.of(context).textTheme.labelMedium!.copyWith(fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.center,
-                      ))),
-              if (widget.book.authorName != null)
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [Text(widget.book.authorName ?? "No name", style: Theme.of(context).textTheme.labelSmall)],
-                )
-            ],
-          ),
-        ));
+      padding: EdgeInsets.all(2),
+      child: Card(
+        margin: EdgeInsets.all(0),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Stack(
+                alignment: AlignmentGeometry.lerp(Alignment.center, Alignment.topCenter, 0.75)!,
+                children: [
+                  // Image or Placeholder
+                  if (url != null && url!.isNotEmpty)
+                    ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: ImageLoader(
+                          url: url!,
+                          fit: BoxFit.cover,
+                          constraints: BoxConstraints.expand(),
+                        )),
+                  if (url == null || url!.isEmpty) _imagePlaceHolder(),
+                  if (url != null && url!.isNotEmpty)
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.center,
+                              end: Alignment.bottomCenter,
+                              colors: [Color.fromARGB(181, 0, 0, 0), Colors.black54],
+                            ),
+                          ),
+                          child: bookTitle()),
+                    ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
