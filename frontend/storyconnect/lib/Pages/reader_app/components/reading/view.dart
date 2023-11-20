5fb7e2993d752e444010e66e357c05b351360337
David Harrington <david.harrington2095@gmail.com>
2023-11-20 05:45:48 -0700

    More conversion work.

--- frontend/storyconnect/lib/Pages/reader_app/components/reading/view.dart
+++ frontend/storyconnect/lib/Pages/reader_app/components/reading/view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:storyconnect/Pages/reader_app/components/reading/state/reading_bloc.dart';
+import 'package:storyconnect/Pages/reader_app/components/ui_state/reading_ui_bloc.dart';
+import 'package:storyconnect/Widgets/loading_widget.dart';
+import 'package:visual_editor/visual-editor.dart';
 
 class ReadingPageView extends StatefulWidget {
   const ReadingPageView({super.key});
@@ -15,6 +19,38 @@ class _readingPageViewState extends State<ReadingPageView> {
         constraints: BoxConstraints(
           maxWidth: 800.0,
         ),
-        child: Container());
+        child: BlocBuilder<ReadingBloc, ReadingState>(
+          buildWhen: (previous, current) {
+            return previous.currentIndex != current.currentIndex ||
+                previous.loadingStruct != current.loadingStruct;
+          },
+          builder: (context, state) {
+            Widget toReturn;
+            if (state.loadingStruct.isLoading) {
+              toReturn = LoadingWidget(loadingStruct: state.loadingStruct);
+            } else {
+              toReturn = Container(
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                  border: Border.all(color: Colors.grey[200]!, width: 1),
+                  color: Colors.white,
+                ),
+                constraints:
+                    BoxConstraints(minHeight: ReadingUIBloc.pageHeight),
+              );
+            }
+            return AnimatedSwitcher(
+                duration: Duration(milliseconds: 500), child: toReturn);
+          },
+        ));
   }
 }
