c35d32df7340f58a77050722ac381fc38c909585
tjvilliard <tjvilliard@gmail.com>
2023-11-28 21:23:24 -0700

    Entered urls for about images

--- frontend/storyconnect/lib/Pages/tutorial/view.dart
+++ frontend/storyconnect/lib/Pages/tutorial/view.dart
@@ -12,22 +12,26 @@ class TutorialPopupWidget extends StatelessWidget {
         title: "Writer Home",
         description:
             "The Writer Home Page is where you can view your stories, and add new ones. To create a new book, click the 'Add New Book' button.",
-        imageUrl: null),
+        imageUrl:
+            "https://firebasestorage.googleapis.com/v0/b/storyconnect-9c7dd.appspot.com/o/about_images%2FScreenshot%202023-11-28%20at%207.43.43%E2%80%AFPM.png?alt=media&token=666a4e01-6ad4-4cd0-8b60-779bfbf5045a"),
     TutorialPageData(
         title: "Book Creation",
         description:
             "The Book Creation Page is where you can add a new book. You can add a title, description, and cover image, among other important details. Once you're done, click the 'Create Book' button.",
-        imageUrl: null),
+        imageUrl:
+            "https://firebasestorage.googleapis.com/v0/b/storyconnect-9c7dd.appspot.com/o/about_images%2FScreenshot%202023-11-28%20at%207.43.43%E2%80%AFPM.png?alt=media&token=666a4e01-6ad4-4cd0-8b60-779bfbf5045a"),
     TutorialPageData(
         title: "Writing Page",
         description:
             "The Writing Page is where you can view your book's details, edit the book, and add new chapters. Also be sure to play with our suite of AI-assisted writing tools, including the 'Continuity Checker' and 'Road-UnBlocker'!",
-        imageUrl: null),
+        imageUrl:
+            "https://firebasestorage.googleapis.com/v0/b/storyconnect-9c7dd.appspot.com/o/about_images%2FScreenshot%202023-11-28%20at%207.46.07%E2%80%AFPM.png?alt=media&token=56844c4b-bb13-40b0-a4e4-12623b8d9189"),
     TutorialPageData(
         title: "Reader Home",
         description:
             "After exiting the Writing Page, in the app bar you'll notice in the app bar a link to the Reader Home Page. There, you can view all of the books available on StoryConnect. You can also search for books by title, author, or genre.",
-        imageUrl: null),
+        imageUrl:
+            "https://firebasestorage.googleapis.com/v0/b/storyconnect-9c7dd.appspot.com/o/about_images%2FBookRec.png?alt=media&token=00e1d8ef-12ba-46b2-972d-ef6e6c2f7770"),
     TutorialPageData(
         title: "Reading Page",
         description:
@@ -37,32 +41,35 @@ class TutorialPopupWidget extends StatelessWidget {
         title: "Library Page",
         description:
             "You will also notice in the app bar, another link to the Library Page. The library page will list all the stories currently in your personal library. From here you can easily read a book or remove it from your library. ",
-        imageUrl: null),
+        imageUrl:
+            "https://firebasestorage.googleapis.com/v0/b/storyconnect-9c7dd.appspot.com/o/about_images%2FLibrary%20Page%20Screenshot.png?alt=media&token=653e8d58-54d7-4995-bbd3-481b929f4359"),
     TutorialPageData(
         title: "RoadUnblocker",
         description:
             "The RoadUnblocker is a tool that helps you get past writer's block. It uses OpenAI's GPT-3 to answer questions and generate suggestions for your story. It even remembers what youve written in previous chapters!",
-        imageUrl: null),
+        imageUrl:
+            "https://firebasestorage.googleapis.com/v0/b/storyconnect-9c7dd.appspot.com/o/about_images%2Fru_fullscreen.png?alt=media&token=90d91564-f500-4111-ab63-b5e98d30fff1"),
     TutorialPageData(
         title: "Continuity Checker",
         description:
             "The Continuity Checker is a tool that helps you keep track of your story's continuity. It can identify contradictions in descriptions of characters, locations, and items in your story.",
-        imageUrl: null),
+        imageUrl:
+            "https://firebasestorage.googleapis.com/v0/b/storyconnect-9c7dd.appspot.com/o/about_images%2Fcontinuity_fullscreen.png?alt=media&token=f6d75add-b220-40cd-a5f8-0643d8b6d581"),
     TutorialPageData(
         title: "Content Tagging",
         description:
             "Content tagging is a machine learning based - specifically for text analysis - feature tool to help generate recommended genres of a book by assessing the content of each chapter of the book.",
         imageUrl: null),
     TutorialPageData(
-    title: "User-Based Book Recommendation ",
-    description:
-        "When user lands on the reader's home page, you can see the list of books recommended by the system using machine learning model based on the user's recorded preferences, specifically based on the book rating aspect.",
-    imageUrl: null),
+        title: "User-Based Book Recommendation ",
+        description:
+            "When user lands on the reader's home page, you can see the list of books recommended by the system using machine learning model based on the user's recorded preferences, specifically based on the book rating aspect.",
+        imageUrl: null),
     TutorialPageData(
-    title: "Content-Based Book Recommendation ",
-    description:
-        "In addition to the user-based book recommendation, users are also provided with recommendation assessed based on the text analysis of the similarities of the content of the books.",
-    imageUrl: null),
+        title: "Content-Based Book Recommendation ",
+        description:
+            "In addition to the user-based book recommendation, users are also provided with recommendation assessed based on the text analysis of the similarities of the content of the books.",
+        imageUrl: null),
   ];
 
   @override
