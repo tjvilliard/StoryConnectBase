7ce52d28018e54ef19f6ce2a057a89b1d3712e37
David Harrington <david.harrington2095@gmail.com>
2023-12-02 11:28:09 -0700

    Commit

--- frontend/storyconnect/lib/Pages/reader_app/components/feedback/components/feedback_input.dart
+++ frontend/storyconnect/lib/Pages/reader_app/components/feedback/components/feedback_input.dart
@@ -29,93 +29,88 @@ class FeedbackInputWidgetState extends State<FeedbackInputWidget> {
       return Container(
           alignment: Alignment.bottomCenter,
           child: Column(
-            verticalDirection: VerticalDirection.down,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Row(children: [
-                    Expanded(
-                        child: Container(
-                      constraints: const BoxConstraints(minHeight: 60),
-                      child: TextField(
-                          controller: _feedbackInputController,
-                          maxLength: 1000,
-                          minLines: 1,
-                          maxLines: 5,
-                          decoration: InputDecoration(
-                              hintText: state.serializer.isSuggestion
-                                  ? "Write your Suggestion Here."
-                                  : "Write your Comment Here."),
-                          onChanged: (_) {
-                            context.read<FeedbackBloc>().add(
-                                FeedbackEditedEvent(
-                                    comment: _feedbackInputController.text));
-                          }),
-                    )),
-                    Column(children: [
-                      Container(
-                          alignment: Alignment.bottomCenter,
-                          child: IconButton(
-                              tooltip: "Set Annotated Text",
-                              icon: const Icon(Icons.message_outlined),
-                              onPressed: () {
-                                FeedbackBloc bloc =
-                                    context.read<FeedbackBloc>();
-
-                                ReadingBloc readingBloc =
-                                    context.read<ReadingBloc>();
-
-                                bloc.add(AnnotationChangedEvent(
-                                    readingBloc: readingBloc,
-                                    offset:
-                                        readingBloc.getSelectionBaseOffset(),
-                                    offsetEnd:
-                                        readingBloc.getSelectionOffsetExtent(),
-                                    text: readingBloc.getSelection()));
-                              })),
-                      Container(
-                          alignment: Alignment.bottomCenter,
-                          child: IconButton(
-                              tooltip: "Submit Feedback",
-                              icon: const Icon(Icons.send),
-                              onPressed: () {
-                                FeedbackBloc bloc =
-                                    context.read<FeedbackBloc>();
-
-                                bloc.add(SubmitFeedbackEvent(
-                                    readingBloc: context.read<ReadingBloc>()));
-
-                                _feedbackInputController.clear();
-                              }))
-                    ])
-                  ])),
-              state.serializer.selection.text!.isEmpty
-                  ? Container()
-                  : Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Transform.scale(
-                              scale: 0.55,
-                              child: IconButton(
-                                icon: const Icon(FontAwesomeIcons.x),
+              verticalDirection: VerticalDirection.down,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Row(children: [
+                      Expanded(
+                          child: Container(
+                              constraints: const BoxConstraints(minHeight: 60),
+                              child: TextField(
+                                  controller: _feedbackInputController,
+                                  maxLength: 1000,
+                                  minLines: 1,
+                                  maxLines: 5,
+                                  decoration: InputDecoration(
+                                      hintText: state.serializer.isSuggestion
+                                          ? "Write your Suggestion Here."
+                                          : "Write your Comment Here."),
+                                  onChanged: (_) {
+                                    context.read<FeedbackBloc>().add(
+                                        FeedbackEditedEvent(
+                                            comment:
+                                                _feedbackInputController.text));
+                                  }))),
+                      Column(children: [
+                        Container(
+                            alignment: Alignment.bottomCenter,
+                            child: IconButton(
+                                tooltip: "Set Annotated Text",
+                                icon: const Icon(Icons.message_outlined),
                                 onPressed: () {
-                                  context
-                                      .read<FeedbackBloc>()
-                                      .add(const ClearAnnotationEvent());
-                                },
-                              ),
-                            ),
-                            const Text("Annotated Text: ")
-                          ],
-                        ),
-                        Text(state.serializer.selection.text!),
-                      ],
-                    )
-            ],
-          ));
+                                  FeedbackBloc bloc =
+                                      context.read<FeedbackBloc>();
+
+                                  ReadingBloc readingBloc =
+                                      context.read<ReadingBloc>();
+
+                                  bloc.add(AnnotationChangedEvent(
+                                      readingBloc: readingBloc,
+                                      offset:
+                                          readingBloc.getSelectionBaseOffset(),
+                                      offsetEnd: readingBloc
+                                          .getSelectionOffsetExtent(),
+                                      text: readingBloc.getSelection()));
+                                })),
+                        Container(
+                            alignment: Alignment.bottomCenter,
+                            child: IconButton(
+                                tooltip: "Submit Feedback",
+                                icon: const Icon(Icons.send),
+                                onPressed: () {
+                                  FeedbackBloc bloc =
+                                      context.read<FeedbackBloc>();
+
+                                  bloc.add(SubmitFeedbackEvent(
+                                      readingBloc:
+                                          context.read<ReadingBloc>()));
+
+                                  _feedbackInputController.clear();
+                                }))
+                      ])
+                    ])),
+                state.serializer.selection.text!.isEmpty
+                    ? Container()
+                    : Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                            Row(children: [
+                              Transform.scale(
+                                  scale: 0.55,
+                                  child: IconButton(
+                                      icon: const Icon(FontAwesomeIcons.x),
+                                      onPressed: () {
+                                        context
+                                            .read<FeedbackBloc>()
+                                            .add(const ClearAnnotationEvent());
+                                      })),
+                              const Text("Annotated Text: ")
+                            ]),
+                            Text(state.serializer.selection.text!),
+                          ])
+              ]));
     });
   }
 }
