ab06863567801867a087c1d9cbb62108bb7839a4
Dallin Burton <dallin.burton@me.com>
2023-12-07 21:03:47 -0700

    Add splash screen

--- frontend/storyconnect/lib/Pages/login/view.dart
+++ frontend/storyconnect/lib/Pages/login/view.dart
@@ -16,43 +16,38 @@ class LoginPageView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
-        floatingActionButton: FilledButton(
-          style: ButtonStyle(
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8)))),
-          onPressed: () {
-            Beamer.of(context).beamToNamed(PageUrls.about);
-          },
-          child: const Padding(
-              padding: EdgeInsets.symmetric(vertical: 8),
-              child: Text("Get to know our Team!")),
+      floatingActionButton: FilledButton(
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+          ),
         ),
-        body: ListView(
-          scrollDirection: Axis.vertical,
-          children: [
-            Center(
-                child: Container(
-              alignment: Alignment.bottomCenter,
-              constraints:
-                  const BoxConstraints(maxWidth: LoginPageConstants.maxWidth),
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                        padding: const EdgeInsets.only(),
-                        child: Text(
-                          style: TextStyle(fontSize: 42, color: charcoalBlue),
-                          "StoryConnect",
-                        )),
-                    const EmailField(),
-                    const PasswordField(),
-                    const LoginButton(),
-                    const RegisterLinkButton(),
-                  ]),
-            ))
-          ],
-        ));
+        onPressed: () {
+          Beamer.of(context).beamToNamed(PageUrls.about);
+        },
+        child: const Padding(padding: EdgeInsets.symmetric(vertical: 8), child: Text("About the Project")),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: LoginPageConstants.maxWidth),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 60),
+                Image.asset("assets/splash.png"),
+                const SizedBox(height: 20),
+                const EmailField(),
+                const PasswordField(),
+                const LoginButton(),
+                const RegisterLinkButton(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
